57ca1c21d814d38b8bc459d358dc3c14e0ed87ce
thorts <thorts@amazon.com>
2021-07-16 21:36:16 +0530

    cdk

--- cdkglue/lib/cdkglue-stack.ts
+++ cdkglue/lib/cdkglue-stack.ts
@@ -4,35 +4,35 @@ import * as iam from '@aws-cdk/aws-iam';
 import * as glue from  '@aws-cdk/aws-glue';
 import * as s3deploy from '@aws-cdk/aws-s3-deployment';
 
-export class CdkglueStack extends cdk.Stack {
+export class CdkStack extends cdk.Stack {
     private readonly newProperty = {};
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-  /*  new s3.Bucket(this, 'MyFirstBucket', {
+    //The code that defines your stack goes here//
+  /* new s3.Bucket(this, 'MyFirstBucket', {
       bucketName: 'test-cdk-s3bucket',
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY
 
     });*/
-
-//create Iam role & policy---------------------
-    const role = new iam.Role(this, 'my-glue-job-role-cdk1', {
+    //create Iam role & policy
+    const role = new iam.Role(this, 'my-glue-job-role-cdk221', {
       assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
     });
 
     const gluePolicy = iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSGlueServiceRole");
-    role.addManagedPolicy(gluePolicy);
+    role.addManagedPolicy(gluePolicy); 
 
-//S3 bucket & add policies, role---------
-    const myBucket = new s3.Bucket(this, 'testcdktestglues3bucketstest1', {
+//S3 bucket, policy---------
+    const myBucket = new s3.Bucket(this, 'testcdktestglues3bucketstest321', {
       versioned: true,
-      bucketName: 'testcdktestglues3buckettest1',
+      bucketName: 'testcdktestglues3buckettest31',
       removalPolicy: cdk.RemovalPolicy.RETAIN,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
       });
-      const bucketPolicy = new s3.BucketPolicy(this, 'bucket-policy-id-2', {
+      const bucketPolicy = new s3.BucketPolicy(this, 'bucket-policy-id-4', {
       bucket: myBucket,
     });
     const s3princpal=  bucketPolicy.document.addStatements(
@@ -44,34 +44,36 @@ export class CdkglueStack extends cdk.Stack {
         resources: [`${myBucket.bucketArn}/*`],
       }),
       );
-     myBucket.grantRead(role);
+    
+    myBucket.grantRead(role);
    
-//Glue database & Crawler-----------------------
+   //Glue database & Crawler
    
     const gluedatabase= new glue.CfnDatabase(this, 'my-glue-database',{
-       catalogId: '366101389009',
+       catalogId: '673189904723',
        databaseInput: {name: 'testcdkdatabase'}
      });
      
-    const gluecrawler = new glue.CfnCrawler(this, 'glue_crawler', {
-      databaseName: 'testcdkdatabase',
-      role: role.roleArn,
-      schedule:{'scheduleExpression':'cron(15 12 * * ? *)'},
-      //recrawlpolicy: {},
-      targets: {
-        s3Targets: [{ path: "s3://testcdktestglues3bucket/" }]
-      }
+     const gluecrawler = new glue.CfnCrawler(this, 'glue_crawler', {
+          databaseName: 'testcdkdatabase',
+          role: role.roleArn,
+          schedule:{'scheduleExpression':'cron(15 12 * * ? *)'},
+          //recrawlpolicy: {},
+          targets: {
+            s3Targets: [{ path: "s3://testcdktestglues3bucket/" }]
+          }
      });
      
-//Glue job---------------------------
-
-    const gluejob= new glue.CfnJob(this, 'my-glue-job', {
+  
+  //Glue job----------------- 
+  const gluejob= new glue.CfnJob(this, 'my-glue-job1', {
     role: role.roleArn,
     command: {
         name: 'pythonshell',
         pythonVersion: '3',
-        scriptLocation: 's3://testcdktestglues3bucket/glue-python-scripts/hi.py'
-        }
+        scriptLocation: 's3://testcdktestglues3bucket/glue-python-scripts/job1.py'
+        },
+      maxRetries: 0
     });
     
     new s3deploy.BucketDeployment(this, 'DeployGlueJobFiles', {
@@ -79,5 +81,46 @@ export class CdkglueStack extends cdk.Stack {
       destinationBucket: myBucket,
       destinationKeyPrefix: 'glue-python-scripts'
     });
+    
+    const gluejob1= new glue.CfnJob(this, 'my-glue-job2', {
+    role: role.roleArn,
+    command: {
+        name: 'pythonshell',
+        pythonVersion: '3',
+        scriptLocation: 's3://testcdktestglues3bucket/glue-python-scripts/job2.py'
+        },
+    maxRetries: 0
+    });
+    
+    new s3deploy.BucketDeployment(this, 'DeployGlueJobFiles1', {
+      sources: [s3deploy.Source.asset('./resources/glue-scripts')],
+      destinationBucket: myBucket,
+      destinationKeyPrefix: 'glue-python-scripts1'
+    });
+    
+   const gluejob2 = new glue.CfnJob(this, 'my-glue-job3', {
+      role: role.roleArn,
+      command: {
+          name: 'pythonshell',
+          pythonVersion: '3',
+          scriptLocation: 's3://testcdktestglues3bucket/glue-python-scripts/job3.py'
+          },
+       maxRetries: 0
+    });
+    
+    new s3deploy.BucketDeployment(this, 'DeployGlueJobFiles2', {
+      sources: [s3deploy.Source.asset('./resources/glue-scripts')],
+      destinationBucket: myBucket,
+      destinationKeyPrefix: 'glue-python-scripts2'
+    });
+    
+    const trigger1 = new glue.CfnTrigger(this, 'trigger1', {
+      actions:[{jobName:gluejob2.name}],
+      name : "trigger1",
+      type: "SCHEDULED",
+      schedule: 'cron(15 12 * * ? *)',
+      startOnCreation: true,
+    });
+//end 
  }
 }
